b79acf8d2411646dab244f74662e9bd82f76fd0e
Aleksandr Soloshenko <capcom2me@gmail.com>
2025-12-31 17:58:35 +0700

    [plugins] add LLM plugin

--- internal/censor/plugins/module.go
+++ internal/censor/plugins/module.go
@@ -4,6 +4,7 @@ import (
 	"github.com/capcom6/censor-tg-bot/internal/censor/plugins/duplicate"
 	"github.com/capcom6/censor-tg-bot/internal/censor/plugins/forwarded"
 	"github.com/capcom6/censor-tg-bot/internal/censor/plugins/keyword"
+	"github.com/capcom6/censor-tg-bot/internal/censor/plugins/llm"
 	"github.com/capcom6/censor-tg-bot/internal/censor/plugins/ratelimit"
 	"github.com/capcom6/censor-tg-bot/internal/censor/plugins/regex"
 	"github.com/go-core-fx/logger"
@@ -20,6 +21,7 @@ func Module() fx.Option {
 			fx.Annotate(regex.Metadata, fx.ResultTags(`group:"metadata"`)),
 			fx.Annotate(forwarded.Metadata, fx.ResultTags(`group:"metadata"`)),
 			fx.Annotate(duplicate.Metadata, fx.ResultTags(`group:"metadata"`)),
+			fx.Annotate(llm.Metadata, fx.ResultTags(`group:"metadata"`)),
 		),
 	)
 }
