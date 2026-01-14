2cbb0a8c32827b9ea0fda47b1ab55da767e2c8ae
Aleksandr Soloshenko <capcom2me@gmail.com>
2026-01-14 20:29:18 +0700

    [bot] improve notificaions messages

--- internal/bot/utils.go
+++ internal/bot/utils.go
@@ -1,7 +1,9 @@
 package bot
 
 import (
+	"html"
 	"strconv"
+	"strings"
 
 	tgbotapi "github.com/go-telegram-bot-api/telegram-bot-api/v5"
 )
@@ -10,5 +12,23 @@ func userToString(user *tgbotapi.User) string {
 	if user.UserName != "" {
 		return "@" + user.UserName
 	}
-	return "<pre>" + strconv.FormatInt(user.ID, 10) + "</pre>"
+
+	name := strings.TrimSpace(html.EscapeString(user.FirstName) + " " + html.EscapeString(user.LastName))
+	if name == "" {
+		name = strconv.FormatInt(user.ID, 10)
+	}
+
+	return "<a href=\"tg://user?id=" + strconv.FormatInt(user.ID, 10) + "\">" + name + "</a>"
+}
+
+func messageToString(message *tgbotapi.Message) string {
+	if message.Text != "" {
+		return message.Text
+	}
+
+	if message.Caption != "" {
+		return message.Caption
+	}
+
+	return ""
 }
